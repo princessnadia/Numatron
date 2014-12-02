9a424de178b1a447e5f6f9ab9d63273e96da6b89
Princess Nadia <finn@finnthemew.twomini.com>
2014-12-01 19:33:42 -0600

    Clarity and another test

--- core/crypt.rb
+++ core/crypt.rb
@@ -7,10 +7,10 @@ end
 def debase64(args,nick,chan,rawargs="",pipeargs="")
 	Base64.decode64(args.to_s)
 end
-addCommand("b64",:base64,"Base64 encode the input.")
-addCommand("deb64",:debase64,"Base64 decode the input.")
-addCommand("base64",:base64,"Base64 encode the input.")
-addCommand("debase64",:debase64,"Base64 decode the input.")
+addCommand("b64",:base64,"Encode input with Base64.")
+addCommand("deb64",:debase64,"Decode input with Base64.")
+addCommand("base64",:base64,"Encode input with Base64.")
+addCommand("debase64",:debase64,"Decode input with Base64.")
 def rot13(args,nick,chan,rawargs="",pipeargs="")
 	args.to_s.tr 'A-Za-z','N-ZA-Mn-za-m'
 end
